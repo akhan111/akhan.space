826fd66a2f9fc05e330f60b120d884fa7e5c27e9
Asad Khan <asadkhanusa98@gmail.com>
2021-02-09 19:03:44 -0600

    custom no-drop cursor

---
+++ scripts/draggable-using-jquery-ui.js
@@ -0,0 +1,23 @@
+
+$(function () {
+	$("a").draggable({
+		revert: "invalid",
+		});
+});
+
+$(function () {
+	$("img").draggable({
+		revert: "invalid",
+		});
+});
+
+// $(function () {
+// 	$("body").draggable({
+// 		revert: "invalid",
+// 		});
+// });
+$(function () {
+	$(".pinterest-feed-container").draggable({
+		revert: "invalid",
+		});
+});
